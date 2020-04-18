9b99be7d1d51620053e47c795cf62f1c5e5c7761
Yifan Song (yifsong) <yifsong@cisco.com>
2020-04-18 21:20:54 +0800

    first code

---
+++ server/src/main/java/com/graden/lily/server/LilyApplication.java
@@ -0,0 +1,13 @@
+package com.graden.lily.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/garden/lily/server/LilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.lily.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
